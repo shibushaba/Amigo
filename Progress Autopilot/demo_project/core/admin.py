efc06994604c3434ae42a1b07904886f301b1e3c
shabas <shibushabas23@gmail.com>
2025-12-19 20:35:04 +0530

    cx

--- Progress Autopilot/demo_project/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Goal, TaskStep, DailyStatus, ActionLog
-
-admin.site.register(UserProfile)
-admin.site.register(Goal)
-admin.site.register(TaskStep)
-admin.site.register(DailyStatus)
-admin.site.register(ActionLog)
